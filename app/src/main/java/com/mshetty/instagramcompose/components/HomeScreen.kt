65344336dcbef8b063cd6a02536b428c4124f9cf
mahesh.shetty <mahesh.shetty@vsodir.onmicrosoft.com>
2022-04-21 17:07:55 +0530

    Feat-> App Tool bar in Android

--- app/src/main/java/com/mshetty/instagramcompose/components/HomeScreen.kt
+++ app/src/main/java/com/mshetty/instagramcompose/components/HomeScreen.kt
@@ -1,31 +1,38 @@
 package com.mshetty.instagramcompose.components
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import com.mshetty.instagramcompose.R
 
 @Composable
 fun HomeScreen() {
-    return Scaffold {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(stringResource(R.string.coming_soon),
-                style = TextStyle(
-                    color = colorResource(id = R.color.textColor)
-                ))
-        }
+    return Scaffold(
+        topBar = { TopAppBar(
+            modifier = Modifier.padding(16.dp,16.dp,0.dp,0.dp),
+            title = { Text(text = stringResource(R.string.instagram), style = MaterialTheme.typography.h1) },
+            backgroundColor = colorResource(id = R.color.white),
+            elevation = 0.dp
+        ) },
+    ) {
+
     }
+}
+
+sealed class MenuAction(@DrawableRes val icon: Int) {
+    object Share : MenuAction(R.drawable.ic_baseline_search_24)
 }
--- app/src/main/java/com/mshetty/instagramcompose/ui/theme/Type.kt
+++ app/src/main/java/com/mshetty/instagramcompose/ui/theme/Type.kt
@@ -2,12 +2,18 @@ package com.mshetty.instagramcompose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.mshetty.instagramcompose.R
 
 // Set of Material typography styles to start with
+
+val fonts = FontFamily(
+    Font(R.font.heading, weight = FontWeight.Bold)
+)
+
 val Typography = Typography(
     body1 = TextStyle(fontFamily = FontFamily.Default,
           fontWeight = FontWeight.Normal,
@@ -21,6 +27,11 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
     ),
 
 )
